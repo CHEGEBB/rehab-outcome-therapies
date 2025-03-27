78a9a4aaa5b9738729c0a076c534fa1fc7c961a5
CHEGEBB <chegephil24@gmail.com>
2025-03-27 19:54:30 +0300

    Add Tailwind CSS plugin to PostCSS configuration

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='bg-red-600'>
+      <h1 className='font-semibold text-4xl'>Waah waaah </h1>
+      
+    </div>
+  )
+}
+
+export default Homepage
